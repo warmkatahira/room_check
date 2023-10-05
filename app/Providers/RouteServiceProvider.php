2853a5ca97827f9b4fea86ee3bb65857027885cd
warmkatahira <t.katahira@warm.co.jp>
2023-10-05 10:26:07 +0900

    first

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/top';
+    public const HOME = '/progress/customer';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
