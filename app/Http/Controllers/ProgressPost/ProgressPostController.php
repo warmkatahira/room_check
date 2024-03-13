fc2c084b8074a93dcea64b6e7e07cdf90a0a5051
warmkatahira <t.katahira@warm.co.jp>
2024-03-13 13:09:16 +0900

    20240313_1

--- app/Http/Controllers/ProgressPost/ProgressPostController.php
+++ app/Http/Controllers/ProgressPost/ProgressPostController.php
@@ -15,30 +15,30 @@ class ProgressPostController extends Controller
     public function post(Request $request)
     {
         /* dd($request->params); */
-        foreach($request->params as $param){
-            $a = $param;
+        foreach($request->item_code as $key => $item_code){
+            // 送信されてきたパラメータの進捗をテーブルから取得
+            $progress = Progress::where('customer_code', $request->customer_code)
+                ->where('item_code', $item_code);
+            // 既に存在する進捗の場合
+            if($progress->count() > 0){
+                // 値を更新
+                $progress->update([
+                    'progress_value' => $request->progress_value[$key],
+                ]);
+            }
+            // 存在しない進捗の場合
+            if($progress->count() == 0){
+                // 進捗を追加
+                Progress::create([
+                    'customer_code' => $request->customer_code,
+                    'item_code' => $item_code,
+                    'progress_value' => $request->progress_value[$key],
+                ]);
+            }
         }
 
 
-        // 送信されてきたパラメータの進捗をテーブルから取得
-        $progress = Progress::where('customer_code', $request->customer_code)
-                ->where('item_code', $request->item_code);
-        // 既に存在する進捗の場合
-        if($progress->count() > 0){
-            // 値を更新
-            $progress->update([
-                'progress_value' => $request->progress_value,
-            ]);
-        }
-        // 存在しない進捗の場合
-        if($progress->count() == 0){
-            // 進捗を追加
-            Progress::create([
-                'customer_code' => $request->customer_code,
-                'item_code' => $request->item_code,
-                'progress_value' => $request->progress_value,
-            ]);
-        }
+        
         // 荷主のupdated_atを更新
         Customer::where('customer_code', $request->customer_code)->update([
             'updated_at' => CarbonImmutable::now(),
