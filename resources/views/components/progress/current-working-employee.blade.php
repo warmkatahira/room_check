6c926173d4ef7c9bb8b27807ab4f7d468b4e985a
warmkatahira <t.katahira@warm.co.jp>
2024-06-05 16:26:29 +0900

    20240605_1

--- resources/views/components/progress/current-working-employee.blade.php
+++ resources/views/components/progress/current-working-employee.blade.php
@@ -3,7 +3,7 @@
     @foreach($employeeCount as $employee)
         <div class="col-span-3 xl:col-span-1 text-center">
             <p class="bg-black text-white">{{ $employee->shortened_base_name }}</p>
-            <p class="bg-white py-1">{{ $employee->working_count }}<span class="text-xs ml-1">人</span></p>
+            <p class="bg-white text-xl py-1">{{ $employee->working_count }}<span class="text-xs ml-1">人</span></p>
         </div>
     @endforeach
 </div>
