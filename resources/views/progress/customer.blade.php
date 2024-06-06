3c0b2edac47195ddad7b14e22497fe8962e04388
warmkatahira <t.katahira@warm.co.jp>
2024-06-06 11:31:04 +0900

    20240606_1

--- resources/views/progress/customer.blade.php
+++ resources/views/progress/customer.blade.php
@@ -1,10 +1,10 @@
 @vite(['resources/js/window_update.js'])
 
 <x-app-layout>
-    <!-- カテゴリ選択 -->
-    <x-progress.category-select />
     <!-- 出勤中人数 -->
     <x-progress.current-working-employee :employeeCount="$employee_count" />
+    <!-- カテゴリ選択 -->
+    <x-progress.category-select />
     <!-- 荷主単位 -->
     <x-progress.arr-data :data="$data" />
 </x-app-layout>
