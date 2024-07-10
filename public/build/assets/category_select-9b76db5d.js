10278a7891d10d3ebdee62ab927b3a4485cf02dc
warmkatahira <t.katahira@warm.co.jp>
2024-07-10 16:21:25 +0900

    20240710_1

---
+++ public/build/assets/category_select-9b76db5d.js
@@ -0,0 +1,1 @@
+$(function(){$(".category_select").each(function(){let e=$(this).attr("href");location.href.indexOf(e)!==-1&&($(this).removeClass("bg-theme-sub"),$(this).addClass("bg-theme-main text-white"))})});$(document).ready(function(){$("#search_base_id").change(function(){$("#base_select_form").submit()})});
--- resources/js/progress/category_select.js
+++ resources/js/progress/category_select.js
@@ -5,7 +5,7 @@ $(function() {
         // 取得した要素のhref属性値を取得
         let href = $(this).attr('href');
         // 取得した要素のhref属性値と現在のURLが同じなら処理を行う
-        if(location.href == href) {
+        if(location.href.indexOf(href) !== -1) {
             // クラスを削除
             $(this).removeClass('bg-theme-sub');
             // クラスを追加
