6dd67608c999a5caaff8690516a3a92aa050a148
warmkatahira <t.katahira@warm.co.jp>
2024-07-10 16:31:53 +0900

    20240710_1

--- resources/views/components/progress/category-select.blade.php
+++ resources/views/components/progress/category-select.blade.php
@@ -6,7 +6,7 @@
     <a href="{{ route('progress.tag') }}" class="category_select col-span-4 xl:col-span-1 text-center py-2 px-5 bg-theme-sub">タグ</a>
     @if($bases)
         <form method="GET" action="{{ route('progress.customer') }}" class="col-span-12 xl:col-span-3 col-start-1 xl:col-start-10 m-0 grid grid-cols-3" id="base_select_form">
-            <label for="search_base_id" class="text-sm text-center bg-theme-main text-white col-span-1 pt-2.5">拠点選択</label>
+            <label for="search_base_id" class="text-sm text-center bg-theme-main text-white col-span-1 pt-2.5"><i class="las la-hand-pointer la-lg mr-1"></i>拠点選択</label>
             <select id="search_base_id" name="search_base_id" class="text-sm col-span-2">
                 <option value="">全て</option>
                 @foreach($bases as $base)
