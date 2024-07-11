efa1a774574a3743bbe7f50ed77e955c460f758d
warmkatahira <t.katahira@warm.co.jp>
2024-07-11 09:21:26 +0900

    20240711_1

--- app/Http/Controllers/ShippingConfirmedPost/ShippingConfirmedPostController.php
+++ app/Http/Controllers/ShippingConfirmedPost/ShippingConfirmedPostController.php
@@ -26,12 +26,18 @@ class ShippingConfirmedPostController extends Controller
                         ->get();
         // 進捗の分だけループ処理
         foreach($progresses as $progress){
+            // 追加する進捗に対応する出荷検品残数があればマイナスする(途中で締める荷主対策　ウエニなど)
+            $inspection_incomplete_progress_value = 0;
+            $inspection_incomplete = Progress::where('customer_code', $request->customer_code)->where('item_code', 'inspection_incomplete_'.$progress->item_code)->first();
+            if($inspection_incomplete){
+                $inspection_incomplete_progress_value = $inspection_incomplete->progress_value;
+            }
             // レコードを追加
             ProgressHistory::create([
                 'date' => CarbonImmutable::now()->format('Y-m-d'),
                 'customer_code' => $request->customer_code,
                 'item_code' => $progress->item_code,
-                'progress_value' => $progress->progress_value,
+                'progress_value' => $progress->progress_value - $inspection_incomplete_progress_value,
             ]);
         }
         return response()->json([
