d23db92c7cacb3378223b78485bf17adb2d5230c
warmkatahira <t.katahira@warm.co.jp>
2024-07-11 10:27:48 +0900

    20240711_1

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -10,21 +10,21 @@
         <table class="text-xs block whitespace-nowrap">
             <thead>
                 <tr class="text-center text-white bg-gray-600 sticky top-0">
+                    <th class="font-thin py-3 px-2">拠点名</th>
                     <th class="font-thin py-3 px-2">荷主コード</th>
                     <th class="font-thin py-3 px-2">荷主分類</th>
                     <th class="font-thin py-3 px-2">荷主名</th>
-                    <th class="font-thin py-3 px-2">拠点名</th>
                 </tr>
             </thead>
             <tbody class="bg-white">
                 @foreach($customers as $customer)
                     <tr class="text-left hover:bg-theme-sub cursor-default">
+                        <td class="py-1 px-2 border">{{ $customer->base->base_name }}</td>
                         <td class="py-1 px-2 border">
                             <a href="{{ route('customer.update_index', ['customer_code' => $customer->customer_code]) }}" class="text-blue-600">{{ $customer->customer_code }}</a>
                         </td>
                         <td class="py-1 px-2 border">{{ $customer->customer_category }}</td>
                         <td class="py-1 px-2 border">{{ $customer->customer_name }}</td>
-                        <td class="py-1 px-2 border">{{ $customer->base->base_name }}</td>
                     </tr>
                 @endforeach
             </tbody>
